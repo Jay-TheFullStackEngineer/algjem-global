4b6a830188f7aeebf0634d69a05a9d2b2b27c653
Jay-TheFullStackEngineer <147441174+Jay-TheFullStackEngineer@users.noreply.github.com>
2024-10-08 10:50:39 +1300

    Pages Up

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Next.js 13, Tailwind, and GSAP!</h1>
+    </main>
+  );
+}
